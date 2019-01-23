ff43a69d43f7331c1839e57ef98adc6287178e24
BrettsSchmidt <brettsschmidt@gmail.com>
2019-01-23 18:26:53 -0500

    Added Webconnector class and WikipediaParser for getting Wiki plots from Wiki link. Program is set to test WikipediaParser.

--- CrispyCrawler/CrispyCrawler/Program.cs
+++ CrispyCrawler/CrispyCrawler/Program.cs
@@ -10,6 +10,15 @@ namespace CrispyCrawler
     {
         static void Main(string[] args)
         {
+            string link = "https://en.wikipedia.org/wiki/The_Wolf_of_Wall_Street_(2013_film)";
+            WikipediaParser wiki = new WikipediaParser();
+            List<string> links = new List<string>();
+            links.Add(link);
+            wiki.AddLinks(links);
+            Task.Run(wiki.GetPlotDataAsync);
+            wiki.GetPlotDataAsync().Wait();
+
+            Console.ReadLine();
         }
     }
 }
---
+++ CrispyCrawler/CrispyCrawler/WebConnector.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrispyCrawler
+{
+    /// <summary>
+    /// Class containing static functions for connecting to the web.
+    /// </summary>
+    class WebConnector
+    {
+        public static async Task<string> GetHTML(string link)
+        {
+
+            using (var client = new HttpClient())
+            {
+                var html = client.GetStringAsync(link);
+                return html.Result;
+            }
+
+        }
+    }
+}
+
